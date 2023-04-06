06aa6e9008d102670ee431253c266d4ee5d33b32
Felipe H Souza <75103710+SouzaHFelipe@users.noreply.github.com>
2023-04-05 23:34:35 -0300

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,16 @@
+'use strict '
+
+const display = document.getElementById('display');
+const numeros = document.querySelectorAll('[id*= tecla ]');
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+
+const atualizarDisplay = (texto) => {
+    display.textContent += texto ;
+}
+
+
+numeros.forEach (
+    numero => numero.addEventListener('click' , inserirNumero)
+);
+
+
