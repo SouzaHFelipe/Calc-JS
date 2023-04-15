862edf9c413bf7bc8e505edc63def9f6b1f099d3
Felipe H Souza <75103710+SouzaHFelipe@users.noreply.github.com>
2023-04-15 18:20:52 -0300

    Update Js.js

--- Js.js
+++ Js.js
@@ -4,10 +4,10 @@ const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*= tecla ]');
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 
+
 const atualizarDisplay = (texto) => {
     display.textContent += texto ;
 }
-
 
 numeros.forEach (
     numero => numero.addEventListener('click' , inserirNumero)
